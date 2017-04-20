b8e3cb738183a64e624a67c3b3cff6fc22be056c
Cody Young <cyoung1623@gmail.com>
2017-04-20 16:21:06 -0700

    Added Term class.

---
+++ src/Term.java
@@ -0,0 +1,17 @@
+/**
+ * Represents a polynomial term as an object.
+ * Created by Cody on 4/20/2017.
+ */
+public class Term {
+    //Instance variables
+    private int coeff;
+    private int exp;
+
+    /**
+     * Constructor method for term objects - coefficient and exponent.
+     */
+    public Term (int c, int e) {
+        coeff = c;
+        exp = e;
+    }
+}
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+
 /**
  * Created by Cody on 4/20/2017.
  */
 public class Tester {
+
+    public static void main (String [] args) throws IOException {
+
+    }
 }
